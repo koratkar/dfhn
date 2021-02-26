de9e5e156149f78f217a94fc6971c55b8aff6a5f
Ishaan <woohooyeshaan@gmail.com>
2021-02-26 13:58:50 -0600

    bugs!

--- df.js
+++ df.js
@@ -1,25 +1,23 @@
 let hn = document.getElementsByTagName("a")
 let burn_words = ['comments', 'discuss', 'new', 'past', 'show', 'jobs', 'submit', 'ask', 'More']
 
-function destroyComments(c) {
+function destroylinks(c) {
     if (c == 0) {
         return
     } else if (typeof(hn[c]) == 'undefined') {
-        destroyComments(c-1)
+        destroylinks(c-1)
     } else {
-        if (hn[c].className !== 'storylink') {
-            for (i=0; i<burn_words.length; i++) {
-                if (hn[c].text.includes(burn_words[i])) {
-                    hn[c].remove()
-                    i = burn_words.length
-                }
+        for (i=0; i<burn_words.length; i++) {
+            if (hn[c].text == (burn_words[i])) {
+                hn[c].remove()
+                i = burn_words.length
             }
         }
-        destroyComments(c-1)
     }
-}
+        destroylinks(c-1)
+    }
 
-function destroyLines() {
+function destroylines() {
     // document.getElementsByClassName("pagetop")[0].innerHtml = '<span class="pagetop"><b class="hnname"><a href="news">Hacker News</a></b></span>'
     // console.log(document.getElementsByClassName("pagetop"))
     let pagetop = document.getElementsByClassName("pagetop")[0]
@@ -30,6 +28,6 @@ function destroyLines() {
     }
 }
 
-destroyComments(hn.length)
-destroyLines()
+destroylinks(hn.length)
+destroylines()
 console.log("DFHN is awake. View the source code: github.com/koratkar/dfhn.")
